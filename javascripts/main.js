9ffc1bd50d59c283fc0859612aeba09e19a95d72
blairvansant <blairbot@gmail.com>
2016-10-31 21:28:00 -0500

    added delete function

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,97 @@
+
 "use strict";
 
+let apiKeys={};
+
+function putTodoInDom(){
+	 FbAPI.getTodos(apiKeys).then(function(items){
+      console.log("items from Firebase", items);
+      $("#completed-tasks").html("");
+      $("#incomplete-tasks").html("");
+
+      items.forEach(function(item){
+      
+      if(item.isCompleted ===true){
+
+
+      let newListItem = '<li>';
+      newListItem+='<div class="col-xs-8">';
+      newListItem+='<input class="checkboxStyle" type="checkbox" checked>';
+      newListItem+=`<label class="inputLabel">${item.task}</label>`;
+      newListItem+='<input type="text" class="inputTask">';
+      newListItem+='</div>';
+      newListItem+='<div class="col-xs-4">';
+      newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
+      newListItem+='<button class="btn btn-danger col-xs-6 delete">Delete</button> ';
+      newListItem+='</div>';
+      newListItem+='</li>';
+      //apend to list
+      $('#completed-tasks').append(newListItem);
+       }else {
+      let newListItem = '<li>';
+      newListItem+='<div class="col-xs-8">';
+      newListItem+='<input class="checkboxStyle" type="checkbox">';
+      newListItem+=`<label class="inputLabel">${item.task}</label>`;
+      newListItem+='<input type="text" class="inputTask">';
+      newListItem+='</div>';
+      newListItem+='<div class="col-xs-4">';
+      newListItem+='<button class="btn btn-default col-xs-6 edit">Edit</button>';
+      newListItem+=`<button class="btn btn-danger col-xs-6 delete" data-fbid="${item.id}">Delete</button> `;
+      newListItem+='</div>';
+      newListItem+='</li>';
+      $('#incomplete-tasks').append(newListItem);
+
+       }
+      });
+    });
+}
+
 $(document).ready(function(){
-	FbAPI.firebaseCredentials().then(function(keys){
-		console.log("keys", keys);
+  FbAPI.firebaseCredentials().then(function(keys){
+    console.log("keys", keys);
+    apiKeys=keys;
+    firebase.initializeApp(apiKeys);
+    putTodoInDom();
+   });
+
+  $("#clicky-button").on("click", function(){
+  	let newItem = {
+  		"task": $("#toDoInput").val(),
+  		"isCompleted": false
+  	};
+
+FbAPI.addTodo(apiKeys, newItem).then(function(){
+	putTodoInDom();
+			});
+
+  	});
+
+$("ul").on("click", ".delete", function(){
+	let itemId = $(this).data("fbid");
+	FbAPI.deleteTodo(apiKeys, itemId).then(function(){
+		putTodoInDom();
+		});
 	});
 
 });
+
+
+
+// "use strict";
+
+// let apiKeys = {};
+
+// $(document).ready(function(){
+// 	FbAPI.firebaseCredentials().then(function(keys){
+// 		console.log("keys", keys);
+// 		apiKeys = keys;
+// 			firebase.initializeApp(apiKeys);
+// 			FbAPI.getTodos(apiKeys).then(function(items){
+// 				console.log("items from FB", items);
+// 			});
+// 	});
+
+// });
 
 
 
